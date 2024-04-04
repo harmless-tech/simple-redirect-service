94857d525e64e008a46e9ee26db0990990f37428
harmless-tech <git@harmless.tech>
2024-04-04 15:59:38 -0400

    Add tracing, fix trailing, validate urls

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@
 #![allow(clippy::multiple_crate_versions)]
 
 use axum::{
-    extract::{Path, State},
+    extract::{Path, Request, State},
     http::StatusCode,
     response::{IntoResponse, Redirect, Response},
     routing::get,
-    Json, Router,
+    Json, Router, ServiceExt,
 };
 use indexmap::IndexMap;
 use serde::Deserialize;
@@ -16,6 +16,10 @@ use std::{
     sync::{Arc, OnceLock},
 };
 use tokio::signal;
+use tower::Layer;
+use tower_http::{normalize_path::NormalizePathLayer, trace, trace::TraceLayer};
+use tracing::{debug, info, trace, Level};
+use url::Url;
 
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
@@ -27,24 +31,46 @@ struct Redirects {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    println!("Starting...");
+    tracing_subscriber::fmt()
+        .with_max_level(Level::TRACE)
+        .with_target(false)
+        .compact()
+        .init();
 
-    // TODO: Check if all URIs are valid.
-    println!("Loading redirects from ./redirects.json...");
+    info!("Starting...");
+
+    info!("Loading redirects from ./redirects.json...");
     let file = fs::read_to_string("./redirects.json")?;
     let redirects: Redirects = serde_json::from_str(&file)?;
-    println!("Found: {redirects:?}");
+    debug!("Found: {redirects:?}");
+
+    debug!("Validating URLs...");
+    for (k, v) in &redirects.redirects {
+        trace!("Validating URL {k} : {v}");
+        Url::parse(v)?;
+    }
+    debug!("Validated URLs.");
 
     let app = Router::new()
         .route("/", get(srs_info))
         .route("/srs", get(srs_src))
         .route("/srs/license", get(srs_license))
         .route("/:path", get(redirect))
+        .fallback(h_404)
         .with_state(Arc::new(redirects));
 
+    let app = app.layer(
+        TraceLayer::new_for_http()
+            .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+            .on_request(trace::DefaultOnRequest::new().level(Level::INFO))
+            .on_response(trace::DefaultOnResponse::new().level(Level::INFO))
+            .on_failure(trace::DefaultOnFailure::new().level(Level::ERROR)),
+    );
+    let app = NormalizePathLayer::trim_trailing_slash().layer(app);
+
     let listener = tokio::net::TcpListener::bind(":::3000").await?;
-    println!("Should be listening soon! On port 3000");
-    axum::serve(listener, app)
+    info!("Should be listening soon! On port 3000");
+    axum::serve(listener, ServiceExt::<Request>::into_make_service(app))
         .with_graceful_shutdown(shutdown_handle())
         .await?;
 
@@ -95,6 +121,10 @@ async fn srs_src() -> Redirect {
 async fn srs_license() -> &'static str {
     static STR: &str = include_str!("../LICENSE");
     STR
+}
+
+async fn h_404() -> impl IntoResponse {
+    (StatusCode::NOT_FOUND, "Invalid URL.")
 }
 
 enum Returns {
